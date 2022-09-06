25cf93955a46821be8b1b697db3b088706ea832f
KhunBall <indeeprw61@gmail.com>
2022-09-06 23:27:18 +0700

    basic 3

--- Basic/app.py
+++ Basic/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
@@ -17,6 +17,12 @@ def admin():
     # ชื่อ อายุ
     username = "KhunBall"
     return render_template("admin.html", username = username)
+
+@app.route('/sendData')
+def signupForm():
+    fname = request.args.get('fname')
+    description = request.args.get('description')
+    return render_template("thankyou.html", data = {"name": fname, "description": description})
 
 if __name__ == '__main__':
     app.run(debug=True)
